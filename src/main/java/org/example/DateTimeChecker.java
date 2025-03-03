48b9feb55efb76982813f09097523b4e45c596bb
Long9904 <longvu09092004@gmail.com>
2025-03-03 14:37:41 +0700

    update code false

--- src/main/java/org/example/DateTimeChecker.java
+++ src/main/java/org/example/DateTimeChecker.java
@@ -12,7 +12,7 @@ public class DateTimeChecker {
             return 30;
         } else if (month == 2) {
             if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-                return 29;
+                return 30;
             } else {
                 return 28;
             }
